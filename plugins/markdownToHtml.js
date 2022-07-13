1d06f2632e368e1dcf8ed7206e4823483eb1b65e
lawliet <evilllawliet@gmail.com>
2022-07-13 15:11:18 +0800

    chore: clean up

--- plugins/markdownToHtml.js
+++ plugins/markdownToHtml.js
@@ -1,8 +1,6 @@
-const remark = require('remark')
 const externalLinks = require('remark-external-links') // Add _target and rel to external links
 const images = require('remark-images') // Improved image syntax
 const unrwapImages = require('remark-unwrap-images') // Removes <p> wrapper around images
-const html = require('remark-html')
 const smartyPants = require('./remark-smartypants') // Cleans up typography
 const customHeaders = require('./remark-header-custom-ids') // Custom header id's for i18n
 
@@ -14,17 +12,4 @@ module.exports = {
     unrwapImages,
     smartyPants,
   ],
-  markdownToHtml,
-}
-
-async function markdownToHtml(markdown) {
-  const result = await remark()
-    .use(externalLinks)
-    .use(customHeaders)
-    .use(images)
-    .use(unrwapImages)
-    .use(smartyPants)
-    .use(html)
-    .process(markdown)
-  return result.toString()
 }
